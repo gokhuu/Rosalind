68c54c09ad6831a091c912fcb3a94f25edebb3bc
Brandon Khuu <47408225+gokhuu@users.noreply.github.com>
2021-06-21 15:35:13 -0400

    as

--- Algorithmic Heights/2sum.py
+++ Algorithmic Heights/2sum.py
@@ -1,17 +1,39 @@
-import os
+def import_file(filename):
+    file = open(filename, 'r')
+
+    lst = []
+
+    for line in file:
+        lst.append(line.strip())
+
+    return lst
 
 
-def sum2(lst):
-    for i in range(len(lst)):
-        for j in range(len(lst)):
+def adjust_file(file):
+    k, n = file[0].split()
+    k, n = int(k), int(n)
+
+    lst = []
+    file = file[1:]
+
+    for line in file:
+        lst.append(line.split())
+
+    return k, n, lst
+
+
+def sum2(n, lst):
+    for i in range(n):
+        for j in range(n):
             if int(lst[i]) == -int(lst[j]):
                 return i, j
     return -1
 
+def iterate_array(k, n, lst):
 
-with open(os.path.join('rosalind_2sum.txt')) as data:
-    n, m = data.readline().strip().split()
-    a = data.readlines()
-    for line in a:
-        for i in line:
-            int(i)
+    return
+
+
+filename = 'rosalind_2sum.txt'
+file = import_file(filename)
+k, n, new_file = adjust_file(file)
